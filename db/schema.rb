3fddd0003604dd19f0707e440b74eca2c76c78a0
nayu7u <yusuke.nadaka@gmail.com>
2022-07-03 23:03:19 +0900

    feat: db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_03_140234) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
