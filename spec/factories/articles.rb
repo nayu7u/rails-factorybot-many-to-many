149ef0023804e60c9b6dbc30a33856e9f77c088a
nayu7u <yusuke.nadaka@gmail.com>
2022-07-04 00:38:47 +0900

    test: articleのFactoryBotで多対多のモデルを作成できるように設定

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :article do
     title { "MyString" }
     text { "MyText" }
+
+    trait :with_tags do
+      after(:build) do |article|
+        tag = create(:tag, name: "for test")
+        article.taggings << build(:tagging, article: article, tag: tag)
+      end
+    end
   end
 end
