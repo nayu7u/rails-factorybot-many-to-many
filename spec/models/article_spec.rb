5d3f33ef473678cb0b7ec9ac311064cfb7e5b98f
nayu7u <yusuke.nadaka@gmail.com>
2022-07-04 00:39:32 +0900

    test: FactoryBotで多対多のインスタンスが作成できることの確認テスト

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "FactoryBotでも多対多のモデルを作成できる" do
+    article = create(:article, :with_tags)
+
+    expect(article.tags.size).to eq 1
+    expect(article.tags.first.name).to eq "for test"
+  end
 end
